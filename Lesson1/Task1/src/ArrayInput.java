6a17c540d908193d0e461bac9b9c5d92953af9cb
namestnikoff <n.a.namestnikov@gmail.com>
2025-09-13 09:33:51 +0300

    Initial commit

---
+++ Lesson1/Task1/src/ArrayInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ArrayInput {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();//читает число
+        System.out.println("Вы ввели " + 56input);
+        scanner.close();
+    }  
+}
