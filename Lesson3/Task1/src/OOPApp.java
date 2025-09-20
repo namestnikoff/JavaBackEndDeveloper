8a4d47c69494bfc8f703d05217d46abc13e5e11f
namestnikoff <n.a.namestnikov@gmail.com>
2025-09-20 22:19:38 +0300

    add input handler

--- Lesson3/Task1/src/OOPApp.java
+++ Lesson3/Task1/src/OOPApp.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
+import utils.InputHandler;
 public class OOPApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int number = InputHandler.getInt(scanner, "Введите число: ", 1, 100);
+        System.out.println("Вы ввели" + number);
     }
 }
